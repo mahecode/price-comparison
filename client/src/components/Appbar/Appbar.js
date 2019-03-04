c336206e2736ce4d48d51d3c054bb93521163039
mahesh maharana <maharanamahesh6@gmail.com>
2019-03-04 21:18:32 +0530

    added android link

--- client/src/components/Appbar/Appbar.js
+++ client/src/components/Appbar/Appbar.js
@@ -4,7 +4,8 @@ import { withStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
-
+import IconButton from '@material-ui/core/IconButton';
+import AndroidIcon from '@material-ui/icons/AndroidRounded';
 
 
 const styles = {
@@ -22,14 +23,20 @@ const styles = {
 
 function ButtonAppBar(props) {
   const { classes } = props;
+  const handleChange = ()=>{
+    window.open("https://drive.google.com/file/d/1Mpn1pYt5MccczfrJlpWNgABj8F7MHKey/view?usp=sharing", '_blank');
+  }
   return (
     <div className={classes.root}>
       <AppBar 
       position="fixed">
         <Toolbar>
-          <Typography variant="h6" color="inherit" className={classes.grow}>
+          <Typography variant="h4" color="inherit" className={classes.grow}>
             Kimatmilao
           </Typography>
+          <IconButton onClick={handleChange} className={classes.menuButton} color="inherit" aria-label="Menu">
+            <AndroidIcon />
+          </IconButton>
         </Toolbar>
       </AppBar>
     </div>
