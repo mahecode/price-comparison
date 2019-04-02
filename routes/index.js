1c78f27de179dc762c6cd3920c34c3532291a4b2
mahesh maharana <maharanamahesh6@gmail.com>
2019-04-02 21:41:58 +0530

    Added grid error

--- routes/index.js
+++ routes/index.js
@@ -15,18 +15,31 @@ router.post('/data',(req,res)=>{
     let amazonData = rp(Queryurl).then(html => {
         let x = -2;
         console.log("Amazon");
-        let innerData = [0,1,2,3].map(element => {
-            return {
-                name: $('.a-size-medium.a-color-base.a-text-normal', html)[element].children[0].data,
-                price: $('.a-price-whole', html)[x = x + 2].children[0].data,
-                imageSrc : $('.a-section.aok-relative.s-image-fixed-height', html).children()[element].attribs['src'],
-                link: Queryurl
-            };
-        });
-        return innerData;
-    }).catch(e => {
-        console.log(e);
-    });
+        try {
+            let innerData = [0,1,2,3].map(element => {
+                return {
+                    name: $('.a-size-medium.a-color-base.a-text-normal', html)[element].children[0].data,
+                    price: $('.a-price-whole', html)[x = x + 2].children[0].data,
+                    imageSrc : $('.a-section.aok-relative.s-image-fixed-height', html).children()[element].attribs['src'],
+                    link: Queryurl
+                };
+            });
+            return innerData;
+        } catch (error) {
+            if(error){
+                let x = -1;
+                let innerData = [0,2,4,6].map( ele => {
+                    return {
+                        name: $('.a-link-normal.a-text-normal', html)[ele].children[0].next.children[0].data,
+                        price: $('.a-price-whole', html)[ele].children[0].data,
+                        imageSrc : $('.a-section.aok-relative.s-image-tall-aspect', html).children()[x = x + 1].attribs['src'],
+                        link: Queryurl
+                    };
+                });
+                return innerData
+            }
+        }
+    })
     const priceCompared = new Promise((resolve,reject)=>{
         productScraper
             .pc(query)
